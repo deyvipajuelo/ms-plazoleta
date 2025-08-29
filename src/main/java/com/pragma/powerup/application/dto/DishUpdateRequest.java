3c925d5cba66cdb22740f2db51557baa423c518f
Deyvi Pajuelo <deyvipajuelo@hotmail.com>
2025-08-29 13:01:12 -0500

    fix(HU-04): add fields validations to DishUpdateRequest

--- src/main/java/com/pragma/powerup/application/dto/DishUpdateRequest.java
+++ src/main/java/com/pragma/powerup/application/dto/DishUpdateRequest.java
@@ -1,7 +1,14 @@
 package com.pragma.powerup.application.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record DishUpdateRequest(
+        @NotNull(message = "El precio es obligatorio")
+        @Positive(message = "El precio debe ser un valor positivo")
         Integer precio,
+        @NotBlank(message = "La descripción es obligatoria")
         String descripcion
 ) {
 }
